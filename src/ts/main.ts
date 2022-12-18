31f419ab9e8f9061af5355aba10680e7c5084631
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2022-12-18 10:30:31 +0100

    mobile-nav

--- src/ts/main.ts
+++
@@ -1,5 +0,0 @@
-const hamburger: HTMLInputElement= document.querySelector(".hamburger") as HTMLInputElement;//Lägga till datatyp
-hamburger.addEventListener("click", function(){
-    this.classList.toggle("close");
-})
-
