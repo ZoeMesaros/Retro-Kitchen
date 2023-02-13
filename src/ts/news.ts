04f12fe0e114ac49ccdb7db62e971031b2de8eb0
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-02-13 20:15:08 +0100

    update

--- src/ts/news.ts
+++ src/ts/news.ts
@@ -1,1 +1,7 @@
 const newsPage = document.getElementById("news") as HTMLDivElement;
+
+const newDiv = document.createElement("div") as HTMLDivElement;
+newDiv.innerHTML = `
+<p>Hej</p>`;
+
+newsPage.append(newDiv);
